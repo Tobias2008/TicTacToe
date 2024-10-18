0d0ad504d0ab7c4c177f64dcb6d13ce6a2e45346
Tobias2008 <68482714+Tobias2008@users.noreply.github.com>
2024-10-18 10:51:49 +0200

    First Game upload

---
+++ script/script.js
@@ -0,0 +1,20 @@
+let player1 = {
+    name: "Player 1",
+    color: "black",
+    weight: 20,
+    score: 0
+}
+
+let player2 = {
+    name: "Player 2",
+    color: "black",
+    weight: 40,
+    score: 0
+}
+
+let gameMatrix = [
+    [-1, -1, -1],
+    [-1, -1, -1],
+    [-1, -1, -1]
+]
+
